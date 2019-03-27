ec3db3b66fbed2c494619972af547656abe5b9a6
oligo87 <oligo@i.ua>
2019-03-27 23:56:56 +0200

    tests added

---
+++ test_book.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+
+
+class TestBooks(unittest.TestCase):
+    def setUp(self):
+        self.base_url = 'http://pulse-rest-testing.herokuapp.com'
+        self.part_url = '/books'
+        self.book_id = None
+
+    def test_book_create(self):
+        book_data = {"title": "Mu-Mu", "author": "Ivan Turgenev"}
+        res = requests.post(self.base_url + self.part_url, data=book_data)
+        self.assertEqual(201, res.status_code)
+        body = res.json()
+        self.book_id = body['id']
+        book_data['id'] = self.book_id
+        self.assertEqual(body, book_data)
+
+    def tearDown(self):
+        if self.book_id is not None:
+            requests.delete(f'{self.base_url}{self.part_url}/{self.book_id}')
---
+++ test_roles.py
@@ -0,0 +1,63 @@
+import unittest
+import requests
+
+from func import post_item
+from func import get_item
+from func import get_list
+from func import put_fields
+from func import del_item
+
+
+class TestRoles(unittest.TestCase):
+    def setUp(self):
+        book = requests.post(base_url+book_part_url, book_data)
+        book = book.json()
+        self.__class__.book_id = book['id']
+
+    def test1_post(self):
+        res, role_id = post_item(base_url, part_url, data)
+        self.__class__.role_id = role_id
+        self.assertEqual(str(res), '<Response [201]>')
+
+    def test2_item(self):
+        res = get_item(self.__class__.role_id, base_url, part_url)
+        exp_res = {'id': self.__class__.role_id, 'name': 'Hero 1', 'type': 'Supernatural', 'level': 5, 'book': 104}
+        self.assertEqual(res, exp_res)
+
+    def test3_item_in_list(self):
+        res = get_list(base_url, part_url)
+        exp_res = "'id': "+str(self.__class__.role_id)+','
+        self.assertIn(exp_res, str(res))
+
+    def test4_put(self):
+        res = put_fields(self.__class__.role_id, base_url, part_url, data_new)
+        self.assertEqual(str(res), '<Response [200]>')
+
+    def test5_item(self):
+        res = get_item(self.__class__.role_id, base_url, part_url)
+        exp_res = {'id': self.__class__.role_id, 'name': 'Hero Changed', 'type': 'Supernatural', 'level': 6,
+                   'book': 104}
+        self.assertEqual(res, exp_res)
+
+    def test6_item_in_list(self):
+        res = get_list(base_url, part_url)
+        exp_res = {'id': self.__class__.role_id, 'name': 'Hero Changed', 'type': 'Supernatural', 'level': 6,
+                   'book': 104}
+        self.assertIn(exp_res, res)
+
+    def test7_delete(self):
+        res = del_item(self.__class__.role_id, base_url, part_url)
+        self.assertEqual(str(res), '<Response [204]>')
+        res = get_item(self.__class__.role_id, base_url, part_url)
+        self.assertEqual(str(res), "{'detail': 'Not found.'}")
+
+
+if __name__ == '__main__':
+    base_url = 'http://pulse-rest-testing.herokuapp.com'
+    part_url = '/roles'
+    data = {'name': 'Hero 1', 'type': 'Supernatural', 'level': 5, 'book': 104}
+    data_new = {'name': 'Hero Changed', 'level': 6}
+    book_data = {"title": "1984", "author": "George Orwell"}
+    book_part_url = '/books'
+
+    unittest.main()
